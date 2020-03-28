19725ee19ee7e2351a2c91a9ce56214bbce88f3f
Luan Nico <luanpotter27@gmail.com>
2020-03-28 13:57:24 -0400

    Organize folders and packages

---
+++ package/README.md
@@ -0,0 +1,3 @@
+# dart_web_audio
+
+A thin wrap over JS audio APIs for Dart/Flutter
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
+FILE=dart_web_audio.dart
+
 cd gen
 
-dart_js_facade_gen lib.dom.audio.ts > web_audio.dart
+dart_js_facade_gen lib.dom.audio.ts > $FILE
 
-echo 'web_audio.dart' | rex -f '(show promiseToFuture;\n)' '$1import "dart:typed_data" show Uint8List, Float32List, ByteBuffer;'
-echo 'web_audio.dart' | rex -f 'Uint8Array' 'Uint8List'
-echo 'web_audio.dart' | rex -f 'Float32Array' 'Float32List'
-echo 'web_audio.dart' | rex -f 'ArrayBuffer' 'ByteBuffer'
+echo $FILE | rex -f '(show promiseToFuture;\n)' '$1import "dart:typed_data" show Uint8List, Float32List, ByteBuffer;'
+echo $FILE | rex -f 'Uint8Array' 'Uint8List'
+echo $FILE | rex -f 'Float32Array' 'Float32List'
+echo $FILE | rex -f 'ArrayBuffer' 'ByteBuffer'
 
-mv web_audio.dart ../lib/
+mv $FILE ../package/lib/
